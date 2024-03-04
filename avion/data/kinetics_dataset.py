a68d5077c761ae795838e00879c0792eda2e8570
Mona Ahmadian <94292893+Moohnai@users.noreply.github.com>
2024-03-04 18:04:51 +0000

    Update kinetics_dataset.py

--- avion/data/kinetics_dataset.py
+++ avion/data/kinetics_dataset.py
@@ -29,36 +29,37 @@ def read_metadata(metadata_fname, root=None, args=None, mode='train'):
                 samples.append((line_info[0], int(line_info[1]), int(line_info[2])))
     return samples
 
-def video_loader_by_frames(
-    root, vid, frame_ids, threads=1,
-    fast_rrc=False, rrc_params=(224, (0.5, 1.0)),
-    fast_msc=False, msc_params=(224,),
-    fast_cc=False, cc_params=(224,),
-    hflip_prob=0., vflip_prob=0.,
-):
-    if fast_rrc:
-        width, height = rrc_params[0], rrc_params[0]
-    elif fast_msc:
-        width, height = msc_params[0], msc_params[0]
-    elif fast_cc:
-        width, height = cc_params[0], cc_params[0]
-    else:
-        width, height = -1, -1
-    vr = decord.VideoReader(
-        os.path.join(root, vid), num_threads=threads,
-        width=width, height=height,
-        use_rrc=fast_rrc, scale_min=rrc_params[1][0], scale_max=rrc_params[1][1],
-        use_msc=fast_msc,
-        use_centercrop=fast_cc,
-        hflip_prob=hflip_prob, vflip_prob=vflip_prob,
-    )
-    try:
-        frames = vr.get_batch(frame_ids).asnumpy()
-    except (IndexError, decord.DECORDError) as error:
-        print(error)
-        print("Erroneous video: ", vid)
-        frames = torch.zeros((len(frame_ids), 240, 320, 3))
-    return torch.from_numpy(frames.astype(np.float32))
+# def video_loader_by_frames(
+#     root, vid, frame_ids, threads=1,
+#     fast_rrc=False, rrc_params=(224, (0.5, 1.0)),
+#     fast_msc=False, msc_params=(224,),
+#     fast_cc=False, cc_params=(224,),
+#     hflip_prob=0., vflip_prob=0.,
+# ):
+#     if fast_rrc:
+#         width, height = rrc_params[0], rrc_params[0]
+#     elif fast_msc:
+#         width, height = msc_params[0], msc_params[0]
+#     elif fast_cc:
+#         width, height = cc_params[0], cc_params[0]
+#     else:
+#         width, height = -1, -1
+#     vr = decord.VideoReader(
+#         os.path.join(root, vid), num_threads=threads,
+#         width=width, height=height,
+#         use_rrc=fast_rrc, scale_min=rrc_params[1][0], scale_max=rrc_params[1][1],
+#         use_msc=fast_msc,
+#         use_centercrop=fast_cc,
+#         hflip_prob=hflip_prob, vflip_prob=vflip_prob,
+#     )
+#     try:
+#         frames = vr.get_batch(frame_ids).asnumpy()
+#     except (IndexError, decord.DECORDError) as error:
+#         print(error)
+#         print("Erroneous video: ", vid)
+#         frames = torch.zeros((len(frame_ids), 240, 320, 3)).numpy()
+#     return torch.from_numpy(frames.astype(np.float32))
+
 
 
 class KineticsDataset(torch.utils.data.Dataset):
@@ -83,11 +84,12 @@ class KineticsDataset(torch.utils.data.Dataset):
                  window_size=(8, 14, 14),
                  mask_ratio=0.9,
                  # for quick prototype
-                 subsample_stride=None):
+                 subsample_stride=None,
+                 args = None,):
         super().__init__()
 
         self.root = root
-        self.samples = read_metadata(metadata)
+        self.samples = read_metadata(metadata, args=args)
         self.full_samples = self.samples.copy()
         if isinstance(subsample_stride, int):
             self.samples = self.samples[::subsample_stride]
@@ -114,13 +116,35 @@ class KineticsDataset(torch.utils.data.Dataset):
             self.masked_position_generator = None
         else:
             raise NotImplementedError
+        
+    def video_loader_by_frames(self,
+        root, vid, threads=1,
+        fast_rrc=False, rrc_params=(224, (0.5, 1.0)),
+        fast_msc=False, msc_params=(224,),
+        fast_cc=False, cc_params=(224,),
+        hflip_prob=0., vflip_prob=0.,
+    ):
+        if fast_rrc:
+            width, height = rrc_params[0], rrc_params[0]
+        elif fast_msc:
+            width, height = msc_params[0], msc_params[0]
+        elif fast_cc:
+            width, height = cc_params[0], cc_params[0]
+        else:
+            width, height = -1, -1
+        # vr = decord.VideoReader(
+        #     os.path.join(root, vid), num_threads=threads,
+        #     width=width, height=height,
+        #     use_rrc=fast_rrc, scale_min=rrc_params[1][0], scale_max=rrc_params[1][1],
+        #     use_msc=fast_msc,
+        #     use_centercrop=fast_cc,
+        #     hflip_prob=hflip_prob, vflip_prob=vflip_prob,
+        # )
+        vr = decord.VideoReader(
+            os.path.join(root, vid), num_threads=threads,
+        )
 
-    def __getitem__(self, i):
-        if self.verbose:
-            print("[{}] __getitem__() starts at {}".format(os.getpid(), datetime.datetime.now()))
-
-        video_id, num_frames, _ = self.samples[i]
-
+        num_frames = len(vr)
         if num_frames > (self.clip_length + 1) * self.clip_stride:
             start_id = np.random.randint(num_frames - (self.clip_length + 1) * self.clip_stride)
         else:
@@ -130,17 +154,33 @@ class KineticsDataset(torch.utils.data.Dataset):
             shift = np.random.randint(self.clip_stride, size=self.clip_length)
             frame_ids += shift
         frame_ids = frame_ids % num_frames
+
+
+        try:
+            frames = vr.get_batch(frame_ids).asnumpy()
+        except (IndexError, decord.DECORDError) as error:
+            print(error)
+            print("Erroneous video: ", vid)
+            frames = torch.zeros((len(frame_ids), 240, 320, 3)).numpy()
+        return torch.from_numpy(frames.astype(np.float32))
+
+    def __getitem__(self, i):
+        if self.verbose:
+            print("[{}] __getitem__() starts at {}".format(os.getpid(), datetime.datetime.now()))
+
+        video_id, _, label, _ = self.samples[i]
+
         if self.is_training:
-            frames = video_loader_by_frames(
-                self.root, video_id + '.mp4' if '.' not in video_id else video_id, frame_ids, threads=self.threads,
+            frames = self.video_loader_by_frames(
+                self.root, video_id + '.mp4' if '.' not in video_id else video_id, threads=self.threads,
                 fast_rrc=self.fast_rrc, rrc_params=self.rrc_params,
                 fast_msc=self.fast_msc, msc_params=self.msc_params,
                 fast_cc=False, cc_params=self.cc_params,
                 hflip_prob=self.hflip_prob, vflip_prob=self.vflip_prob,
             )
         else:
-            frames = video_loader_by_frames(
-                self.root, video_id + '.mp4' if '.' not in video_id else video_id, frame_ids, threads=self.threads,
+            frames = self.video_loader_by_frames(
+                self.root, video_id + '.mp4' if '.' not in video_id else video_id, threads=self.threads,
                 fast_rrc=False, rrc_params=self.rrc_params,
                 fast_cc=True, cc_params=self.cc_params,
                 hflip_prob=0., vflip_prob=0.,
@@ -154,7 +194,7 @@ class KineticsDataset(torch.utils.data.Dataset):
             print("[{}] __getitem__() end at {}".format(os.getpid(), datetime.datetime.now()))
 
         if self.masked_position_generator is None:
-            return frames
+            return frames, label
         else:
             return frames, self.masked_position_generator()
 
